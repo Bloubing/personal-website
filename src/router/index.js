6b9b5e41234222da4f8661f1f8c3e43d6220ce8f
Bloubing <140849997+Bloubing@users.noreply.github.com>
2025-09-27 16:46:55 +0200

    fix: 404 apres rechargement page non racine

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@ import ProjectInnovGuide from '@/pages/projects/ProjectInnovGuide.vue'
 import ProjectPersonalWebsite from '@/pages/projects/ProjectPersonalWebsite.vue'
 import ProjectRecipes from '@/pages/projects/ProjectRecipes.vue'
 import ProjectSelfHosting from '@/pages/projects/ProjectSelfHosting.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
